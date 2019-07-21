0e2f34eb1dbb451612bb34b460933e7980eeaadf
zcWSR <zhaocong@zcwsr.com>
2019-07-22 00:17:52 +0800

    fix: babel-register 非工程目录下运行时, 无法正确读取babelrc配置的问题

--- www.js
+++ www.js
@@ -1,3 +1,8 @@
-require('@babel/register');
+// eslint-disable
+const path = require('path');
+require('@babel/register')({
+  extends: path.resolve(__dirname, '.babelrc'),
+  ignore: [/node_modules/]
+});
 require('@babel/polyfill');
 require('./index');

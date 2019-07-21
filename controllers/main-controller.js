93e1851396f42692d388765a49a4e237f8241e39
zcWSR <zhaocong@zcwsr.com>
2019-07-22 00:42:04 +0800

    fix: 彩蛋

--- controllers/main-controller.js
+++ controllers/main-controller.js
@@ -6,7 +6,7 @@ class MainController {
   main() {
     let message = '<h1>a simple command based OSU! game info searching qq-bot</h1>';
     message += '<h2>get more info on my <a href="https://github.com/zcWSR/japari-admin">github</a></h2>';
-    message += '<h2><a href="">⇨ジャパリパーク⇦</a></h2>';
+    message += '<h2><a href="/japari">⇨ジャパリパーク⇦</a></h2>';
     return message;
   }
 }

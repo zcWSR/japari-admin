3837e45bd8142a7e0ee9cb64e58cf0e5312877fe
zcWSR <zhaocong@zcwsr.com>
2019-11-15 20:24:00 +0800

    chore: sh

--- build-before-push.sh
+++ build-before-push.sh
@@ -3,12 +3,13 @@ echo 'build before push......'
 npm run build
 git add ./built
 
-git status -s ./built
-changedFile=$?
-if [ ! $changeFile ]; then
+changedFile=`git status -s ./built | grep '^[MARCD]'`
+echo $changeFile
+
+if [ -z "$changedFile" ]; then
+  echo 'file not changed, ignore'
+else
   echo 'file changed, do commit'
   commit="build: $(date '+%Y-%m-%d %H:%M:%S')"
-  git commit -m "${commit}" --allow-empty
-else
-  echo 'file not changed, ignore'
+  git commit -m "${commit}"
 fi

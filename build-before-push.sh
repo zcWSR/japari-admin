8085388358eb2aadefc1adcd62e141db9a3af7f5
zcWSR <zhaocong@zcwsr.com>
2019-11-15 19:58:14 +0800

    chore: sh

--- build-before-push.sh
+++ build-before-push.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
-echo 'build before push'
+echo 'build before push......'
 npm run build
-git add .
-commit="build: $(date '+%Y-%m-%d %H:%M:%S')"
-git commit -m "${commit}" --allow-empty
+git add ./built
+
+git status -s ./built
+changedFile=$?
+if [ ! $changeFile ]; then
+  echo 'file changed, do commit'
+  commit="build: $(date '+%Y-%m-%d %H:%M:%S')"
+  git commit -m "${commit}" --allow-empty
+else
+  echo 'file not changed, ignore'
+fi

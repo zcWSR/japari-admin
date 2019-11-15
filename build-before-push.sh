26b8e09eac3cc02ca5cb6ef26321c586ff351fb6
zcWSR <zhaocong@zcwsr.com>
2019-11-15 18:32:53 +0800

    build: 2019-11-15 18:32:49

--- build-before-push.sh
+++ build-before-push.sh
@@ -3,6 +3,6 @@ echo 'build before push'
 npm run build
 git add .
 commit="build: $(date '+%Y-%m-%d %H:%M:%S')"
-git commit -m "${commit}"
+git commit -m "${commit}" --allow-empty
 git push origin master
 exit 0

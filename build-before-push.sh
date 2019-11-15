e1f2205b52c8abe71388b11be094fba06c0bec70
zcWSR <zhaocong@zcwsr.com>
2019-11-15 18:25:47 +0800

    chore: 修复build脚本

--- build-before-push.sh
+++ build-before-push.sh
@@ -2,5 +2,6 @@
 echo 'build before push'
 npm run build
 git add .
-commit = "build: $(date '+%Y-%m-%d %H:%M:%S')"
-git commit -m $commit
+commit="build: $(date '+%Y-%m-%d %H:%M:%S')"
+git commit -m "${commit}"
+git push origin $(git_current_branch)

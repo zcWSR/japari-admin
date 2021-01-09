9989f2c2e219f453014990ad2c3fd4aa1f037904
zcWSR <zhaocong@zcwsr.com>
2021-01-09 21:31:58 +0800

    fix: build

--- built/test/index.js
+++
@@ -1,3 +0,0 @@
-"use strict";var _akhr = _interopRequireDefault(require("./akhr"));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}
-
-(0, _akhr.default)();

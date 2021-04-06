c783fd379ae569f9072d0b705a7192c2d6ac9877
zcWSR <zhaocong@zcwsr.com>
2021-04-06 11:30:30 +0800

    build: 2021-04-06 11:30:30

--- built/services/schedule-service.js
+++ built/services/schedule-service.js
@@ -1,5 +1,5 @@
 "use strict";Object.defineProperty(exports, "__esModule", { value: true });exports.default = void 0;var _momentTimezone = _interopRequireDefault(require("moment-timezone"));
-var _nodeScheduleTz = _interopRequireWildcard(require("node-schedule-tz"));
+var _nodeSchedule = _interopRequireWildcard(require("node-schedule"));
 var _logger = _interopRequireDefault(require("../utils/logger"));
 var _qqService = _interopRequireDefault(require("./qq-service"));function _getRequireWildcardCache() {if (typeof WeakMap !== "function") return null;var cache = new WeakMap();_getRequireWildcardCache = function _getRequireWildcardCache() {return cache;};return cache;}function _interopRequireWildcard(obj) {if (obj && obj.__esModule) {return obj;}if (obj === null || typeof obj !== "object" && typeof obj !== "function") {return { default: obj };}var cache = _getRequireWildcardCache();if (cache && cache.has(obj)) {return cache.get(obj);}var newObj = {};var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor;for (var key in obj) {if (Object.prototype.hasOwnProperty.call(obj, key)) {var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null;if (desc && (desc.get || desc.set)) {Object.defineProperty(newObj, key, desc);} else {newObj[key] = obj[key];}}}newObj.default = obj;if (cache) {cache.set(obj, newObj);}return newObj;}function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}function _slicedToArray(arr, i) {return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest();}function _nonIterableRest() {throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");}function _unsupportedIterableToArray(o, minLen) {if (!o) return;if (typeof o === "string") return _arrayLikeToArray(o, minLen);var n = Object.prototype.toString.call(o).slice(8, -1);if (n === "Object" && o.constructor) n = o.constructor.name;if (n === "Map" || n === "Set") return Array.from(o);if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen);}function _arrayLikeToArray(arr, len) {if (len == null || len > arr.length) len = arr.length;for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i];return arr2;}function _iterableToArrayLimit(arr, i) {if (typeof Symbol === "undefined" || !(Symbol.iterator in Object(arr))) return;var _arr = [];var _n = true;var _d = false;var _e = undefined;try {for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) {_arr.push(_s.value);if (i && _arr.length === i) break;}} catch (err) {_d = true;_e = err;} finally {try {if (!_n && _i["return"] != null) _i["return"]();} finally {if (_d) throw _e;}}return _arr;}function _arrayWithHoles(arr) {if (Array.isArray(arr)) return arr;}function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) {try {var info = gen[key](arg);var value = info.value;} catch (error) {reject(error);return;}if (info.done) {resolve(value);} else {Promise.resolve(value).then(_next, _throw);}}function _asyncToGenerator(fn) {return function () {var self = this,args = arguments;return new Promise(function (resolve, reject) {var gen = fn.apply(self, args);function _next(value) {asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value);}function _throw(err) {asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err);}_next(undefined);});};}
 
@@ -101,7 +101,11 @@ class ScheduleService {constructor() {this.
   runSchedule(groupId, name, ruleString, text) {const _this$getRuleFromStri =
     this.getRuleFromString(ruleString),hours = _this$getRuleFromStri.hours,days = _this$getRuleFromStri.days,rule = _this$getRuleFromStri.rule;
     _logger.default.info(`rule '${rule}'`);
-    (0, _nodeScheduleTz.scheduleJob)(name, rule, 'Asia/Shanghai', this.sendText.bind(null, groupId, text));
+    (0, _nodeSchedule.scheduleJob)(
+    name,
+    { rule, tz: 'Asia/Shanghai' },
+    this.sendText.bind(this, groupId, text));
+
     return { hours, days };
   }
 
@@ -121,7 +125,7 @@ class ScheduleService {constructor() {this.
   }
 
   cancelSchedule(name) {
-    const job = _nodeScheduleTz.default.scheduledJobs[name];
+    const job = _nodeSchedule.default.scheduledJobs[name];
     if (job) {
       job.cancel();
     }

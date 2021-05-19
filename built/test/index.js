9f5f47948e9adbab0591702465a4cf6908d6c55d
zcWSR <zhaocong@zcwsr.com>
2021-05-19 18:53:18 +0800

    build: 2021-05-19 18:53:18

--- built/test/index.js
+++ built/test/index.js
@@ -6,5 +6,4 @@ var _fontLoader = _interopRequireDefault(require("../plugins/font-loader"));
 var _hoshiiService = _interopRequireDefault(require("../services/hoshii-service"));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };} // import test from './akhr';
 // test();
 new _fontLoader.default().init();
-// HoShiiService.drawImage('这是一条', '测试语句');
-console.log(_hoshiiService.default.drawImage('这是一条', 'emoji👍🏻测试'));
+_hoshiiService.default.drawImage('这是一条', '测试语句');

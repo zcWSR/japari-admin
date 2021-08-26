7b2091b588595cb63b137e377bd6be75a5e268b5
zcWSR <zhaocong@zcwsr.com>
2021-08-26 16:57:23 +0800

    chore: 适配 ci

--- built/test/index.js
+++
@@ -1,3 +0,0 @@
-"use strict";var _akhrService = _interopRequireDefault(require("../services/akhr-service"));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}
-
-_akhrService.default.updateAndFormate();

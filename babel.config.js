b47077e6d0c3c2e061448c9fe07890cdcc81f638
zcWSR <zhaocong@zcwsr.com>
2023-04-23 19:00:42 +0800

    chore: update package.json

--- babel.config.js
+++ babel.config.js
@@ -16,6 +16,6 @@ module.exports = {
   ],
   plugins: [
     ['@babel/plugin-proposal-decorators', { legacy: true }],
-    ['@babel/plugin-proposal-class-properties', { loose: true }]
+    ['@babel/plugin-proposal-class-properties', { loose: false }]
   ]
 };
